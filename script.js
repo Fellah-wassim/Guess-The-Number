c7bf2bd5056c91dd8c1570e90a3c45fa0a224a5a
wassim <wbvb41@gmail.com>
2022-08-02 21:25:28 +0200

    fixed some style when pressing again button

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.number').textContent = '?';
   document.querySelector('.message').textContent = 'Start guessing...';
   document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.score').textContent = 20;
+  document.querySelector('.score').textContent = score;
   document.querySelector('.guess').value = '';
+  document.querySelector('.number').style.width = '15rem';
 });
 // document.querySelector('.message').textContent = '☑️ Correct number!';
 // document.querySelector('.number').textContent = number;
