2296e6b8258539a6d7ba6f3f1ab3251830d0272c
wassim <wbvb41@gmail.com>
2022-08-02 18:40:41 +0200

    added display message while playing the game

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 'use strict';
 const number = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 // document.querySelector('.message').textContent = '☑️ Correct number!';
 // document.querySelector('.number').textContent = number;
 // document.querySelector('.score').textContent = 10;
 // document.querySelector('.guess').value = 23;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  if (!guess) {
+  if (score === 1) {
+    document.querySelector('.message').textContent = 'GAME OVER';
+    document.querySelector('.score').textContent = 0;
+  } else if (!guess) {
     document.querySelector('.message').textContent = '🛑 No number Entered!';
   } else {
     if (number === guess) {
       document.querySelector('.message').textContent = '☑️ Correct number!';
     } else {
-      document.querySelector('.score').textContent--;
+      score--;
+      document.querySelector('.score').textContent = score;
       if (guess > number) {
         document.querySelector('.message').textContent = 'Too high!';
       } else {
